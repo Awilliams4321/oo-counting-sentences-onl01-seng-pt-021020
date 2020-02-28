7ab9de0d816243f54c7505fb740fbc28d92a9952
Amber Williams <awilliams1234@live.com>
2020-02-28 01:51:50 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,19 @@ require 'pry'
 class String
 
   def sentence?
+<<<<<<< HEAD
   self.end_with?(".")
   end
 
   def question?
     self.end_with?("?")
+=======
+  self.end_with?("!")
+  end
+
+  def question?
+    self.end_with("?")
+>>>>>>> eb8795200e06d99fd6e0b2846c0213edd6a7fdd0
   end
 
   def exclamation?
