14d0c1ba88cc2b2c5c3a60c413bcb9655788a4dc
Amber Williams <awilliams1234@live.com>
2020-02-28 01:52:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,26 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-<<<<<<< HEAD
-  self.end_with?(".")
+    self.end_with?(".")
   end
 
   def question?
     self.end_with?("?")
-=======
-  self.end_with?("!")
   end
-
-  def question?
-    self.end_with("?")
->>>>>>> eb8795200e06d99fd6e0b2846c0213edd6a7fdd0
-  end
-
+  
   def exclamation?
     self.end_with?("!")
   end
-
+  
   def count_sentences
-    self.split(/[.!?]/).reject {|x| x.empty?}.size 
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
 end 
